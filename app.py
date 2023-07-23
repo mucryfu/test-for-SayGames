19b87e581ee8631312516085a98a162947e876ec
Polina Kim <polina.kim.itmo@gmail.com>
2023-07-23 22:54:14 +0100

    new

--- app.py
+++ app.py
@@ -517,6 +517,12 @@ if option == 'Top-5':
     a = 5
 if option == 'Top-10':
     a = 10
+
+df_p['country'] = df_p['country'].astype(str)
+df_p['level'] = df_p['level'].astype(int)
+df_p['gun_name'] = df_p['gun_name'].astype(str)
+df_p['users'] = df_p['users'].astype(int)
+df_p['percentage'] = df_p['percentage'].astype(float)
 df_p = df_p.groupby(['level', 'gun_name']).mean()
 df_p = df_p.drop('users', axis=1)
 df_p.rename(columns = {'percentage':'popularity'}, inplace = True )
